949c0e577d1f89ac129f98b7fdc671dbc916301c
Binod Bhandary <binod.bhandary@nicaisabank.com>
2023-07-19 19:32:05 +0545

    final ur fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -70,7 +70,7 @@ class HomeController extends Controller
             ],
             "consumerAuthenticationInformation" => [
                 "referenceId" => "64827f74-99fa-4109-842e-0dbfc9738876",
-                "returnUrl" => "https://chilaxhouse.com.np/webook/confirm-api-pay-redirect"
+                "returnUrl" => "https://chilaxhouse.com.np/webhook/confirm-api-pay-redirect"
             ],
             "processingInformation" => [
                 "capture" => true,

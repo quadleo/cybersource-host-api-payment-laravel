4aa3e65f8e7d34fea4d0356169e5822da3e34a0b
Binod Bhandary <binod.bhandary@nicaisabank.com>
2023-07-19 19:21:12 +0545

    update in url

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'webhook/*', // Disable CSRF protection for routes matching the "webhook/*" pattern
     ];
 }
